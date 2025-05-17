25d075974c9976d1abc0df6940d24171968ca031
Sai Sathwik Konjerla <saisathwikkonjerla2006@gmail.com>
2025-05-17 21:29:16 +0530

    added the signup, login, logout fields

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,17 @@
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+
+import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import  { connectDB }  from "./lib/db.js";
 
-
-
 const app =express();
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/api/auth", authRoutes);
 
