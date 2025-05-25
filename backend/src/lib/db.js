1a821a92b519f63f3d05be1c3ddec0e040c83548
Sai Sathwik Konjerla <saisathwikkonjerla2006@gmail.com>
2025-05-25 22:04:37 +0530

    done

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,8 +5,6 @@ import { resolve } from "path";
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 10000, // 10 seconds timeout
             socketTimeoutMS: 45000, // 45 seconds socket timeout
             tls: true, // Enable TLS/SSL
